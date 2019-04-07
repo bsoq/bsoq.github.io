ba2dec5d58f72bd32d4a8dbeb71f9eb0376f859e
Fiddlekins <fiddlekins@gmail.com>
2019-04-07 19:12:35 +0100

    Rushed some code for listing stories

---
+++ index.js
@@ -0,0 +1,20 @@
+async function convertToTitle(threadId) {
+    const response = await fetch(`/story/posts/${threadId}.json`);
+    const post = response.json();
+    document.getElementById(`threadLink${threadId}`).innerHTML += ` - ${post.subject}`;
+}
+
+async function listStories() {
+    const response = await fetch('/story/threads/index.json');
+    const index = response.json();
+    const storyList = document.getElementById('storyList');
+    for (const storyType of Object.keys(index)) {
+        let typeHtml = `<h3>${storyType}</h3>`;
+        for (const { threadId } of index[storyType]) {
+            typeHtml += `<a href="/story/threads/${threadId}.html" id="threadLink${threadId}">${threadId}</a><br>`;
+        }
+        storyList.innerHTML += typeHtml;
+    }
+}
+
+listStories().catch(console.error);
